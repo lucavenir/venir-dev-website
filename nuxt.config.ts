709f92a7fc72423feaa7956c3d042ea9747b1ac4
venir.dev <luca@venir.dev>
2024-03-28 15:22:57 +0100

    cleaned up some more

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,11 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
-    '@nuxtjs/tailwindcss',
     '@nuxt/content',
     '@nuxtjs/i18n',
     '@pinia/nuxt',
     '@vueuse/nuxt',
     '@nuxt/image',
-    '@nuxtjs/color-mode',
     '@nuxtjs/google-fonts',
   ]
 })
