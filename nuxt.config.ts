71c020dfb93f6b68eac2e29d3c257ff63ab073a4
venir.dev <luca@venir.dev>
2024-03-28 15:49:04 +0100

    configured w/ nuxt-ui-pro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  extends: ['@nuxt/ui-pro'],
   modules: [
+    '@nuxt/ui',
     '@nuxt/content',
     '@nuxtjs/i18n',
     '@pinia/nuxt',
