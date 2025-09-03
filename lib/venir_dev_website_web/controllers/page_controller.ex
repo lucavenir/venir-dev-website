6057cc166b09da2e36f2a28be44afd01b00e135b
~venir <luca.venir@wde.it>
2025-09-03 15:53:35 +0200

    solved SEO meta tags

--- lib/venir_dev_website_web/controllers/page_controller.ex
+++ lib/venir_dev_website_web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule VenirDevWebsiteWeb.PageController do
   use VenirDevWebsiteWeb, :controller
 
   def home(conn, _params) do
-    render(conn, :home, page_title: "home")
+    render(conn, :home,
+      page_title: "home",
+      page_description: "~venir's personal website, an business-minded software engineer"
+    )
   end
 end
