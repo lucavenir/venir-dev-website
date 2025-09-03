8ec56d743b02f2cd2c899a9d71a5132c0da61a68
~venir <luca.venir@wde.it>
2025-09-03 10:58:21 +0200

    a better page title, I'd say

--- lib/venir_dev_website_web/controllers/page_controller.ex
+++ lib/venir_dev_website_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule VenirDevWebsiteWeb.PageController do
   use VenirDevWebsiteWeb, :controller
 
   def home(conn, _params) do
-    render(conn, :home)
+    render(conn, :home, page_title: "home")
   end
 end
